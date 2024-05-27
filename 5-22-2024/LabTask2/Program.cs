f5915e8b1f75ef76afb4531e268e1b234e9c04dd
hasan <86876834+hasanhmbt@users.noreply.github.com>
2024-05-27 20:31:58 +0400

    New Code Added or updated
    some changes to the code base : 2024-05-27-20-31-57

---
+++ 5-22-2024/LabTask2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace LabTask2
+{
+
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+             EvenOrOddNumber();
+           //IsMinusOrPlus();
+           //CalculateGPA();
+        }
+
+
+        public static void EvenOrOddNumber()
+        {
+            Console.WriteLine("Enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+             
+
+            if (num > 0 || num % 2 == 0)
+                Console.WriteLine($"{num} is an even number.");
+            else
+                Console.WriteLine($"{num} is an odd number.");
+
+        }
+
+
+        public static void IsMinusOrPlus()
+        {
+            Console.WriteLine("Enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num != 0 || num < 0)
+                Console.WriteLine($"{num} is a minus number.");
+            else if (num == 0)
+                Console.WriteLine(" 0 not is a pluse or minus number.");
+            else
+                Console.WriteLine($"{num} is a pluse number.");
+        }
+
+        public static void CalculateGPA()
+        {
+            Console.WriteLine("Enter a number:");
+            int GPA = Convert.ToInt32(Console.ReadLine());
+
+            if (GPA > 90 && GPA <= 100)
+                Console.WriteLine($"Your GPA is A");
+            else if (GPA > 80 && GPA <= 90)
+                Console.WriteLine($"Your GPA is B");
+            else if (GPA > 70 && GPA <= 80)
+                Console.WriteLine($"Your GPA is C");
+            else if (GPA > 60 && GPA <= 70)
+                Console.WriteLine($"Your GPA is D");
+            else if (GPA > 50 && GPA <= 60)
+                Console.WriteLine($"Your GPA is E");
+            else if ( GPA<51)
+                Console.WriteLine($"Your GPA is F");
+
+        }
+    }
+
+
+
+}
---
+++ 5-27-2024/ClassTask4(Arrays)/Program.cs
@@ -0,0 +1,130 @@
+﻿namespace ClassTask4_Arrays_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetAverage();
+            //GetMinIndex();
+            FindSimpleOrComplex();
+        }
+
+
+        #region GetAverage
+
+        public static void GetAverage()
+        {
+
+            int[] numbers = { 4, 5, 6, 7 };
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            double average = (double)sum / numbers.Length;
+
+            Console.WriteLine($"Array average is: {average}");
+        }
+        #endregion
+
+        #region GetMinIndex
+
+
+        public static void GetMinIndex()
+        {
+
+            int[] numbers = { 1, 0, 50, -11, 23, 18 };
+
+            int min = numbers[0];
+            int minIndex = 0;
+
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                    minIndex = i;
+                }
+            }
+            Console.WriteLine($"The minimum number is: {min} \n");
+            Console.WriteLine($"The index of the minimum number is: {minIndex}\n ");
+
+
+
+
+            Console.Write($"Numbers of Array:");
+
+            for (int i = 0; i < numbers.Length; i++)
+                Console.Write($"{numbers[i]}, ");
+
+
+            Console.WriteLine(Environment.NewLine, Environment.NewLine);
+
+        }
+        #endregion
+
+
+        #region FindSimpleOrComplex
+
+
+        public static void FindSimpleOrComplex()
+        {
+
+            int simpleCount = 0;
+            int complexCount = 0;
+
+            Console.Write("Enter size of the array:");
+            int arrLength = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter array's elements:");
+            int[] nums = new int[arrLength];
+
+
+            for (int i = 0; i < arrLength; i++)
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int num in nums)
+            {
+                if (IsSimple(num))
+                    simpleCount++;
+                else
+                    complexCount++;
+            }
+
+
+            int[] simples = new int[simpleCount];
+            int[] complexes = new int[complexCount];
+
+            int complexIndex = 0; int simpleIndex = 0;
+
+            foreach (int num in nums)
+            {
+                if (IsSimple(num))
+                    simples[simpleIndex++] = num;
+                else
+                    complexes[complexIndex++] = num;
+            }
+            //Console.Clear();
+            Console.WriteLine("Simple numbers:" + String.Join(',', simples));
+            Console.WriteLine("Complex numbers:" + String.Join(',', complexes));
+
+        }
+
+        public static bool IsSimple(int num)
+        {
+            if (num <= 1) return false;
+
+            for (int i = 2; i < num; i++)
+                if (num % i == 0) return false;
+
+            return true;
+        }
+        #endregion
+
+
+    }
+}
+
